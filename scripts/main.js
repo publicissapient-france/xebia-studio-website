1f2406721f427bcb34ed1d857ff762dee3aadfee
Benoit Lemoine <blemoine@xebia.fr>
2014-07-29 11:33:43 +0200

    Correction indicateur

--- scripts/main.js
+++ scripts/main.js
@@ -7,18 +7,25 @@ $(function () {
         var currentUrl = window.location.href;
         var filename = currentUrl.substring(currentUrl.lastIndexOf('/') + 1);
 
+        var currentLi = null
         $('nav li').each(function () {
             var $li = $(this);
 
             if ($li.find('a').attr('href') == filename) {
+                currentLi = $li;
 
-                $li.addClass('selected');
-
-                var $indicator = $('.indicator');
-                var indicatorPosition = $li.position().left + $li.outerWidth() / 2 - $indicator.outerWidth() / 2;
-                $indicator.css('left', indicatorPosition);
             }
         });
+
+        var $indicator = $('.indicator');
+        if (currentLi) {
+            currentLi.addClass('selected');
+
+            var indicatorPosition = currentLi.position().left + currentLi.outerWidth() / 2 - $indicator.outerWidth() / 2;
+            $indicator.css('left', indicatorPosition);
+        } else {
+            $indicator.css('visibility','hidden');
+        }
     };
 
     positionMenu();
